c84c5b1ffe72bd11035fd090dedddddca85cdc07
sulav <sulavmaharjan63@gmail.com>
2020-12-10 16:31:02 +0545

    Added schedult

--- run_this.py
+++ run_this.py
@@ -1,4 +1,17 @@
 from utils.zodiac import SendMail
+import schedule
+import time
 
 
-SendMail('gemini', 'sulavmaharjan63@gmail.com')
+def job():
+    SendMail('gemini', 'sulavmaharjan63@gmail.com')
+    print('Email sent')
+
+
+job()
+schedule.every().day.at("07:00").do(job)
+
+
+while True:
+    schedule.run_pending()
+    time.sleep(2)
